0676b49e0162f91dc81ef62c7ae5bfe910342311
Diener Dornelas <dienezim@hotmail.com>
2022-10-08 16:06:06 -0300

    fix text button submit

--- src/app/pages/Login/SignUp/index.tsx
+++ src/app/pages/Login/SignUp/index.tsx
@@ -179,7 +179,7 @@ export function SignUp({ handleFlip }: SignUpProps) {
                 className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-30"
                 disabled={!isSubmitting}
               >
-                Sign in
+                Sign Up
                 <span className="flex items-center pl-3">
                   {!isSubmitting && (
                     <LockClosedIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
