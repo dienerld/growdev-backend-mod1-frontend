ec0250c95bc92734b1816746f0b3bb2a673e55a6
Diener Dornelas <dienezim@hotmail.com>
2022-10-12 20:40:37 -0300

    conditional load task

--- src/app/pages/Private/Layout.tsx
+++ src/app/pages/Private/Layout.tsx
@@ -1,17 +1,18 @@
 import { useEffect } from 'react';
-import {
-  Outlet, useNavigate,
-} from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 
 import { useAppDispatch, useAppSelector } from '@redux/hooks';
 import { isLogged, userActions } from '@redux/modules/user';
-import { Navbar } from './components/Navbar';
+import { selectorTasks, taskActions } from '@/app/redux/modules/tasks';
+
 import { axios } from '@/app/services/axios';
-import { taskActions } from '@/app/redux/modules/tasks';
 import { ResponseUser } from '@/@types/app';
 
+import { Navbar } from './components/Navbar';
+
 export function Private() {
-  const user = useAppSelector((state) => state.user);
+  const { user } = useAppSelector((state) => state);
+  const totalTasks = useAppSelector(selectorTasks.selectTotal);
   const dispatch = useAppDispatch();
   const redirect = useNavigate();
 
@@ -20,18 +21,18 @@ export function Private() {
       try {
         if (user.remember) {
           dispatch(userActions.login(user));
-
-          //
         } else {
-          const { payload: logged } = (await dispatch(isLogged()));
-          if (!logged) { redirect('/'); }
+          const logged = (await dispatch(isLogged())).payload as boolean;
+          !logged && redirect('/');
         }
 
-        const { data } = await axios.get<ResponseUser>(
-          '/users',
-          { headers: { Authorization: `Bearer ${user.token}` } },
-        );
-        dispatch(taskActions.addManyTasks(data.tasks));
+        if (totalTasks === 0) {
+          const { data } = await axios.get<ResponseUser>(
+            '/users',
+            { headers: { Authorization: `Bearer ${user.token}` } },
+          );
+          dispatch(taskActions.addManyTasks(data.tasks));
+        }
       } catch (err) {
         redirect('/');
       }
@@ -40,7 +41,7 @@ export function Private() {
 
   return (
     <>
-      <Navbar />
+      <Navbar settings={[]} />
       <Outlet />
     </>
   );
