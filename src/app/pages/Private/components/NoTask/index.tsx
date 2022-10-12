b9eb462a4afe07291fece48e7e147fe7559dfe9b
Diener Dornelas <dienezim@hotmail.com>
2022-10-12 20:38:57 -0300

    create page with no task

--- src/app/pages/Private/components/NoTask/index.tsx
+++ src/app/pages/Private/components/NoTask/index.tsx
@@ -1,5 +1,11 @@
+import { Typography } from '@mui/material';
+
 export function NoTask() {
   return (
-    <div className="dark:text-orange-300 text-blue-900"> Sem Task</div>
+    <div className="flex flex-1 justify-center content-center items-center mt-20">
+      <Typography variant="h4" component="h4">
+        No tasks yet
+      </Typography>
+    </div>
   );
 }
