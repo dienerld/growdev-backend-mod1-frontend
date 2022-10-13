52fecd18d87674701bb921143e29ec572b9eaa73
Diener Dornelas <dienezim@hotmail.com>
2022-10-13 19:02:29 -0300

    add dynamic data

--- src/app/pages/Private/components/Task/index.tsx
+++ src/app/pages/Private/components/Task/index.tsx
@@ -1,38 +1,51 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Checkbox, Typography } from '@mui/material';
+import {
+  CalendarMonthOutlined as CalendarMonthIcon,
+  EditOutlined as EditIcon,
+  DeleteOutlineOutlined as DeleteIcon,
+  QueryBuilderOutlined as ClockIcon,
+} from '@mui/icons-material';
+
+import { TTask } from '@/@types/app';
 
 type TTaskProps = {
-  title: string;
+  task: TTask;
 };
 
-export function Task({ title }: TTaskProps) {
+export function Task({ task }: TTaskProps) {
   return (
     <Box
-      className="grid grid-cols-12 rounded-3xl"
+      className="grid grid-cols-12 rounded-3xl p-4"
       sx={{
         backgroundColor: 'background.primary',
+        color: 'text.primary',
       }}
     >
-      <Typography
-        className="col-span-6"
-        variant="h6"
-        sx={{
-          fontSize: '1.5rem',
-          fontWeight: 600,
-          mb: 2,
-        }}
-      >{title}
+
+      <Box className="col-span-5 flex items-center">
+        <Checkbox
+          inputProps={{ 'aria-label': 'controlled' }}
+          size="small"
+          checked={task.done}
+        />
+        <Typography className="ml-1">
+          {task.title}
+        </Typography>
+      </Box>
+
+      <Typography className="hidden sm:flex col-span-3 items-center justify-center" variant="body2">
+        <CalendarMonthIcon className="mr-2" />
+        {task.date.toLocaleDateString()}
       </Typography>
 
-      <Typography className="col-span-2">
-        Date
+      <Typography className="hidden sm:flex col-span-2 items-center justify-center" variant="body2">
+        <ClockIcon className="mr-2" />
+        {task.hour}
       </Typography>
 
-      <Typography className="col-span-2">
-        Hour
-      </Typography>
-
-      <Typography className="col-span-2">
-        buttons
+      <Typography className="hidden sm:flex col-span-2 items-center justify-end gap-4">
+        <EditIcon fontSize="small" color="warning" />
+        <DeleteIcon fontSize="small" color="error" />
       </Typography>
 
     </Box>
